b621daeafae9cd9f2b5ff10b853d552730a8b486
AimenYaseen <aimenyaseen6@gmail.com>
2023-07-21 23:30:00 +0500

    handled boundary/edge case

--- homework_5/10_memoization/fibonacci_dp.cpp
+++ homework_5/10_memoization/fibonacci_dp.cpp
@@ -19,6 +19,12 @@ long long Fib(int n, vector<int>& Vs){
 
 long long Fib(int n)
 {
+    if(n <= 0)
+    {
+        cout << "n must start from 1" << endl ;
+        return -1;
+    }
+
     vector<int> Vs(n, {-1});
     // this fibonacci starts from 0
     Vs[0] = 0;  // F0
@@ -29,6 +35,8 @@ long long Fib(int n)
 
 
 int main(){
+    cout << "0th Fibonacci: " << Fib(0) << endl; // -1
+    cout << "1st Fibonacci: " << Fib(1) << endl; // 0
     cout << "5th Fibonacci: " << Fib(5) << endl; // 0 1 1 2 3
     cout << "10th Fibonacci: " << Fib(10) << endl; // 0 1 1 2 3 5 8 13 21 34
 }
