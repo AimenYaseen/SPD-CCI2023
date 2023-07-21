30f6f2d5488a2c7ea2936a1df99319c74e8b7f4b
AimenYaseen <aimenyaseen6@gmail.com>
2023-07-21 22:14:32 +0500

    print digits in both directions

--- homework_5/1_digit_by_digit/digit_by_digit.cpp
+++ homework_5/1_digit_by_digit/digit_by_digit.cpp
@@ -3,25 +3,41 @@ using namespace std;
 #include <iostream>
 
 
-void print_left_to_right(int num)
+void print_left_to_right(int num) // 123, 12, 1
 {
-    if(num == 0){
+    if(num == 0)  // base case
+    { 
         return;
     }
 
-    print_left_to_right(num / 10);
-    cout << num % 10 << " ";
+    print_left_to_right(num / 10); // pop from stack - last in first out
+    cout << num % 10 << " "; // 1 2 3
 }
 
 
-void print_right_to_left(int num)
+void print_right_to_left(int num) // 123, 12, 1
 {
-    if(num == 0){
+    if(num == 0) // base case
+    {
         return;
     }
 
-    cout << num % 10 << " ";
-    print_right_to_left(num / 10);
+    cout << num % 10 << " ";  // 3 2 1
+    print_right_to_left(num / 10);  // pop from stack
+}
+
+
+void print_both_sides(int num){
+    if(num == 0)
+    {
+        return;
+    }
+
+    cout << num % 10 << " "; // right to left
+
+    print_both_sides(num / 10);
+    
+    cout << num % 10 << " "; // left to right
 }
 
 
@@ -32,5 +48,9 @@ int main(){
 
     cout << "---------- PRINT RIGHT TO LEFT --------------";
     print_right_to_left(123);  // 3 2 1
+    cout << endl;
+
+    cout << "---------- PRINT IN BOTH DIRECTIONS --------------";
+    print_both_sides(123);
     cout << endl;
 }
