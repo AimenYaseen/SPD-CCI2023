91bcc5df81cd0c023eacb4678ff9b66854b9d022
AimenYaseen <aimenyaseen6@gmail.com>
2023-08-01 05:09:46 +0500

    calculate nth term of this series

--- homework_4/question_1/part_2/3_series/4_.cpp
+++ homework_4/question_1/part_2/3_series/4_.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 int series_4(int n){
     // base case
-    if(n == 0){
+    if(n == 1){
         return 1;
     }
     
-    return pow(3, n) + series_4(n-1);
+    return pow(3, n-1) + series_4(n-1);
 }
 
 
 int main(){
-    // return the sum of n+1 elements using this series 1+3+9+27+81+...+3^N
-    // here n starts from 0
-    cout << series_4(5) << endl; // 1 + 3 + 9 + 27 + 81 + 243 = 364
+    // return the sum of n elements using this series 1+3+9+27+81+...+3^N
+    // here n starts from 1
+    cout << series_4(5) << endl; // 1 + 3 + 9 + 27 + 81 = 121
 }
