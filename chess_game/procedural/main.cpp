d4d0e2137cf63c7e1f3168d2865a20b1e17f5dd0
AimenYaseen <aimenyaseen6@gmail.com>
2023-08-23 06:23:27 +0500

    chess main/menu file

---
+++ chess_game/procedural/main.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+
+#include <array>
+#include <iostream>
+#include "functions.h"
+
+
+int main()
+{
+    cout << "LET'S PLAY CHESS" << endl;
+
+    string playerNames[2];
+    char board[8][8];
+    int turn;
+    int sr, sc, er, ec;
+
+    // initialization
+    
+}
