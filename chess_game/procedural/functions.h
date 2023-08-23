ef8558522eb88504cfdf2fa834e6bfd5b67ce9d7
AimenYaseen <aimenyaseen6@gmail.com>
2023-08-23 06:46:34 +0500

    initialization according to playes are done

--- chess_game/procedural/functions.h
+++ chess_game/procedural/functions.h
@@ -19,9 +19,46 @@ void getPlayerNames(string playerNames[])
     cout << endl;
 }
 
+void setBoardElements(char board[][8], char E[], int r1, int r2)
+{
+    int cl = 0, cr = 7;
+
+    for(int i=0; i<=3; i++)
+    {
+        // set queen and king
+        if(i == 3)
+        {
+            board[r1][cl] = E[i];
+            board[r1][cr] = E[i+1];
+        }
+        else
+        {
+            board[r1][cl] = E[i];
+            board[r1][cr] = E[i];
+            cl++;
+            cr--;
+        }
+    }
+
+    // set pawns
+    for(int c = 0; c <= 7; c++)
+        board[r2][c] = E[5];
+}
+
 void initializeBoard(char board[][8])
 {
+    // Initializing each element with an empty string
+    for (int i = 0; i < 8; ++i)
+        for (int j = 0; j < 8; ++j)
+            board[i][j] = ' ';
+        
+    // ------------ BLACK WILL BE SMALL --------------
+    char BE[6] = {'c', 'r', 'b', 'q', 'k', 'p'};
+    setBoardElements(board, BE, 0, 1);
 
+    // ------------ WHITE WILL BE CAPITAL ------------
+    char WE[6] = {'C', 'R', 'B', 'Q', 'K', 'P'};
+    setBoardElements(board, WE, 7, 6);
 }
 
 
@@ -31,4 +68,6 @@ void init(string playerNames[], char board[][8], int& turn)
     getPlayerNames(playerNames);
 
     initializeBoard(board);
+
+    turn = WHITE;
 }
